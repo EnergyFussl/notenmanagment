86142afa34619fa2b8114cfefa2462d924dee497
Lukas Friedl <lukas@friedl.li>
2018-02-28 19:25:50 +0100

    Sql Creation

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    user: "notenmanagment",
+    password: "0pVDuG3OG8gi50lu",
+    database: "notenmanagment"
+
+})
+
+connection.connect(function(err){
+    if(err){
+        console.log("SQL Connection error:" + err)
+        return
+    }
+    console.log("connected to DB")
+})
+
+app.get('/api/hello',function(req,res) {
+    console.log('api entry called')
+    //res.type('text/plain').send('hello back, how <b>are</b> you?')
+    res.status(403).type('text/plain').send('hello back, how <b>are</b> you?')
+})
+
+// Auf diesen Port antwortet der Server
+app.listen(3000,function() {
+    console.log('server running and listening on port 3000')
+})
