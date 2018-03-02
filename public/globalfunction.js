d9ba54a22f8488bcdf11653029ef4223ec6708dd
Loama99 <alexander.leimer@htl-braunau.at>
2018-03-02 14:45:48 +0100

    Edited index.html and index.js

--- public/globalfunction.js
+++ public/globalfunction.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
     navBar()
     //Klassendropdown()
+    $("p").mouseover(function(){
+      $("p").css("background-color", "lightgray");
+    });
+    $("p").mouseout(function(){
+      $("p").css("background-color", "white");
+    });
   })
   
   
-function navBar() {
-    let gen = ''
-    gen += '<nav class=" navbar-material darken-3"><div class="nav-wrapper"><a href="/calendar" class="brand-logo right"></a><ul id="nav-mobile" class="left"></ul></div></nav>'
-    document.getElementById('navDiv').innerHTML = gen;
-  }
+  function navBar() {
+    
+      let gen = ''
+    
+      gen += '<nav class="teal navbar-material darken-3"><div class="nav-wrapper"><a href="" class="brand-logo right"><img class="responsive-img" src="HTL-Logo-schwarz-transparent.png"></a><ul id="nav-mobile" class="left">'+ activeTab() +'</ul></div></nav>'
+    
+      document.getElementById('navDiv').innerHTML = gen;
+    
+    }
--- public/index.js
+++ public/index.js
@@ -1,19 +1,33 @@
 
 test=[
-    {"Typ":"Test", "Fach":"Fsst", "Info":"Datenbank","Datum":"20.2.2018","Notendurchschnitt":"3,2"},
-    {"Typ":"SA", "Fach":"Mathe","Datum":"15.2.2018","Notendurchschnitt":"2,2"},
-    {"Typ":"SMÜP", "Fach":"Englisch","Datum":"20.2.2018","Notendurchschnitt":"2,8"},
-    {"Typ":"Test", "Fach":"KSN","Datum":"24.2.2018","Notendurchschnitt":"3,6"}
+    {"TID":"0","Typ":"Test", "Fach":"Fsst", "Info":"Datenbank","Datum":"20.2.2018","Notendurchschnitt":"3,2"},
+    {"TID":"1","Typ":"SA", "Fach":"Mathe","Datum":"15.2.2018","Notendurchschnitt":"2,2"},
+    {"TID":"2","Typ":"SMÜP", "Fach":"Englisch","Datum":"20.2.2018","Notendurchschnitt":"2,8"},
+    {"TID":"3","Typ":"Test", "Fach":"KSN","Datum":"24.2.2018","Notendurchschnitt":"3,6"}
 ]
 noten=[
-    {"Name":"Sepp", "Note":"4","Punkte":"12/20","Bemerkung":""},
-    {"Name":"Hans", "Note":"","Punkte":"","Bemerkung":"gefehlt"},
-    {"Name":"Franz", "Note":"4","Punkte":"10/20","Bemerkung":"-"},
-    {"Name":"Hias", "Note":"2","Punkte":"19/20","Bemerkung":""}
+    {"SID":"0","Name":"Sepp", "Note":"4","Punkte":"12/20","Bemerkung":""},
+    {"SID":"1","Name":"Hans", "Note":"","Punkte":"","Bemerkung":"gefehlt"},
+    {"SID":"2","Name":"Franz", "Note":"4","Punkte":"10/20","Bemerkung":"4-"},
+    {"SID":"3","Name":"Hias", "Note":"2","Punkte":"19/20","Bemerkung":""}
 ]
-
+fach =[
+    {"FID":"0","Name":"Fsst"},
+    {"FID":"1","Name":"Mathe"},
+    {"FID":"2","Name":"Englisch"},
+    {"FID":"3","Name":"KSN"}
+]
 init()
+//<a onclick="goBack()"class="btn waves-effect waves-light"><i class="material-icons middle">arrow_back</i></a></li>
 
+function activeTab() {
+    
+      return '<li class="active"><li><a href="./index.html" class="waves-effect waves-light btn"><i class="material-icons left">home</i>Home</a></li><li><a href="#info">KlassenInfo</a></li><li><a href="#suche">Suche</a></li><li><a href="#hinz">Test Hinzufügen</a></li>'
+    
+}
+function goBack() {
+    window.history.back();
+}
 function init() {
 
     //updatePersonData()
@@ -22,18 +36,18 @@ function init() {
 
 function klassepressed(buttonEl){
     let divel = document.getElementById('TestTabelle')
-    let htmlTxt = '<table class="striped"> <thead> <tr> <th>Typ</td> <td>Fach</th> <th>Datum</th> <th>Notendurchschnitt</th> <th> </th> </tr> </thead> <tbody>'
+    let htmlTxt = '<div class="container"><div class ="col"><table class="striped"> <thead> <tr> <th>Typ</td> <td>Fach</th> <th>Datum</th> <th>Notendurchschnitt</th> <th> </th> </tr> </thead> <tbody>'
     for(i =0;i<test.length;i++){
         htmlTxt+="<tr> <td>"+test[i].Typ+"</td> <td>"+test[i].Fach+"</td> <td>"+test[i].Datum+"</td> <td>"+test[i].Notendurchschnitt+"</td> <td><button onclick='testclicked()'>Ansehen</button> </td> </tr>"
     }
-    htmlTxt+= " </tbody> </table>"
+    htmlTxt+= " </tbody> </table></div></div>"
     divel.innerHTML=htmlTxt
 }
 
-function Klassendropdown(){
+/*function Klassendropdown(){
      // console.log(buttonEl)
       
-      let htmlTxt='<li><a onclick="klassepressed(this)" href="#!">1AHELS</a></li> <li><a onclick="klassepressed(this)" href="#!">2AHELS</a></li> <li><a onclick="klassepressed(this)" href="#!">3AHELS</a></li> <li><a onclick="klassepressed(this)" href="#!">4AHELS</a></li> <li><a onclick="klassepressed(this)" href="#!">5AHELS</a></li>'
+      let htmlTxt='<li><a onclick="klassepressed(this)" href="Klassenauswahl">1AHELS</a></li> <li><a onclick="klassepressed(this)" href="#!">2AHELS</a></li> <li><a onclick="klassepressed(this)" href="#!">3AHELS</a></li> <li><a onclick="klassepressed(this)" href="#!">4AHELS</a></li> <li><a onclick="klassepressed(this)" href="#!">5AHELS</a></li>'
       let divEl=document.getElementById('dropdownAHELS')
       divEl.innerHTML=htmlTxt  
 }
@@ -41,13 +55,63 @@ function klassepressedtest(buttonEl){
     let divel = document.getElementById('TestTabelle')
     let htmlTxt = '<ul class="collapsible" data-collapsible="accordion"> <li> <div class="collapsible-header"> <i class="material-icons">filter_drama</i> KSN <span class="new badge">4</span></div> <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div> </li> <li> <div class="collapsible-header"> <i class="material-icons">place</i> Englisch <span class="badge">1</span></div> <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div> </li> </ul>'
     divel.innerHTML=htmlTxt
-}
+}*/
 function testclicked(){
     let divel = document.getElementById('TestTabelle')
-    let htmlTxt = '<table class="striped"> <thead> <tr> <th>Name</td> <td>Note</th> <th>Punkte</th> <th>Bemerkung</th> </tr> </thead> <tbody>'
+    let htmlTxt = '<div class="container"><div class ="col"><table class="striped"> <thead> <tr> <th>Name</th> <th>Note</th> <th>Punkte</th> <th>Bemerkung</th> </tr> </thead> <tbody>'
     for(i =0;i<test.length;i++){
-        htmlTxt+="<tr> <td>"+noten[i].Name+"</td> <td>"+noten[i].Note+"</td> <td>"+noten[i].Punkte+"</td> <td>"+noten[i].Bemerkung+"</td></tr>"
+        htmlTxt+="<tr> <td onclick='namepressed("+noten[i].SID+")'>"+"<p>"+noten[i].Name+"</p>"+"</td> <td>"+noten[i].Note+"</td> <td>"+noten[i].Punkte+"</td> <td>"+noten[i].Bemerkung+"</td></tr>"
     }
-    htmlTxt+= " </tbody> </table>"
+    htmlTxt+= " </tbody> </table></div></div>"
     divel.innerHTML=htmlTxt
+}
+function namepressed(namenid){
+    console.log(namenid)
+    let divel = document.getElementById('TestTabelle')
+    let htmlTxt = '<div class="container"><div class ="col"><table class="striped"> <thead> <tr> <th>Fach</th> </tr> </thead> <tbody>'
+    for(i =0;i<fach.length;i++){
+        htmlTxt+="<tr> <td onclick='fachpressed("+namenid+","+fach[i].FID+")'>"+fach[i].Name+"</td></tr>"
+    }
+    htmlTxt+= " </tbody> </table></div></div>"
+    divel.innerHTML=htmlTxt
+
+  
+}
+function fachpressed(name,fachauswahl){
+    console.log(name)
+    console.log(fachauswahl)
+
+    let fachsch
+    for(let i =0;i<fach.length;i++){
+        if(fachauswahl==fach[i].FID){
+            fachsch=fach[i].Name;
+        }
+    }
+    let namesch
+    
+    for(let i =0;i<noten.length;i++){
+        if(name==noten[i].SID){
+            namesch=noten[i].Name;
+        }
+    }
+    console.log(fachsch)
+    
+    console.log(namesch)
+    let htmlTxt="<div class='container'><div class ='col'><table class='striped'> <thead> <tr> <th>Typ</td> <th>Datum</th> <th>Note</th> <th>Punkte</th> <th>Bemerkung</th> </tr> </thead> <tbody>"
+      
+    for(let i =0;i<test.length;i++){
+        if(test[i].Fach==fachsch){
+            htmlTxt+="<tr> <td>"+test[i].Typ+"</td> <td>"+test[i].Datum+"</td>"
+            for(let j=0;j<noten.length;j++){
+                if(namesch==noten[j].Name){
+                    htmlTxt+="<td>"+noten[j].Note+"</td> <td>"+noten[j].Punkte+"</td> <td>"+noten[j].Bemerkung+"</td> </tr>"
+                }
+            } 
+            
+        }
+    }
+    htmlTxt+=" </tbody> </table></div></div>"
+    let divEl=document.getElementById('TestTabelle')
+    divEl.innerHTML=htmlTxt
+
 }
